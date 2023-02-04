6c8c4f9b6693686c7d41c3faaad61900952fa529
f1reball <jacksontbooth@icloud.com>
2023-02-04 19:05:20 +1100

    added boilerplate fetch api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get("https://www.woolworths.com.au/apis/ui/PiesCategoriesWithSpecials")?
+        .text()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
